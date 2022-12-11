3e5b09d7d14f9d8c7aedd31bb300325eac62f9ba
ankomochi114 <s.kotomi.0114@gmail.com>
2022-12-11 19:34:15 +0900

    チュートリアル3の実行

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
-<script>
-export default {
-  data() {
-    return {
-      title: 'My New Vue Title',
-      message: 'Welcome to Vue',
-      isRed: true,
-      user: {
-        firstName: 'John',
-        lastName: 'Smith',
-      },
-    }
-  },
-  computed: {
-    fullName() {
-      return this.user.firstName + ' ' + this.user.lastName
-    },
-  },
-}
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Ref } from 'vue';
+
+const todos: Ref<String[]> = ref([]);
+const doneTodos: Ref<String[]>  = ref([]);
+
+const form = ref(true);
+const inputText = ref("");
+const loading = ref(false);
+const errors = ref("");
+
+/** 
+ * タスクの登録処理 
+*/
+const onSubmit = () => {
+  
+  if (!form) return;
+
+  if (inputText.value.length === 0)
+    return (errors.value = "文字を入力してください");
+  
+  loading.value = true;
+  todos.value.unshift(inputText.value);
+  inputText.value = "";
+  loading.value = false;
+};
+
+/** 
+ * 完了済みのタスクを削除
+ * @return 新しいtodoリスト
+ */
+const clearDoneTodos = () => {
+  return todos.value = todos.value.filter((todo) => doneTodos.value.every((element) => element !== todo))
+};
 </script>
 
 <template>
-  <h1 :title="message" :class="{ red: isRed }">{{ title }}</h1>
-  <h2>{{ fullName }}さんのデータ</h2>
-  <p>Name: {{ fullName }}</p>
+  <v-container>
+    <h1>My Todo App</h1>
+    <v-container v-if="todos.length === 0">
+        <v-alert
+        v-if="todos.length === 0"
+        density="compact"
+        type="info"
+        variant="tonal"
+        >
+        ToDoがまだありません。
+        </v-alert>
+    </v-container>
+    <v-container v-if="errors.length !== 0">
+        <v-alert
+            v-if="errors.length !== 0"
+            density="compact"
+            type="error"
+            variant="tonal"
+        >
+            {{ errors }}
+        </v-alert>
+    </v-container>
+    <v-form v-model="form" @submit.prevent="onSubmit">
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-text-field
+            v-model="inputText"
+            clearable
+            label="Label"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-btn
+            color="success"
+            class="mr-4"
+            type="submit"
+            :loading="loading"
+          >
+            追加
+          </v-btn>
+          <v-btn
+            color="primary"
+            class="mr-4"
+            :loading="loading"
+            @click="clearDoneTodos"
+          >
+            完了済みを削除する
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+    <v-checkbox
+      v-for="todo in todos"
+      v-model="doneTodos"
+      :label="todo"
+      :value="todo"
+    ></v-checkbox>
+  </v-container>
 </template>
+
+<style>
+body {
+  background-color: #eee;
+}
+</style>
---
+++ src/Tutorial2.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: 'My New Vue Title',
+      message: 'Welcome to Vue',
+      isRed: true,
+      user: {
+        firstName: 'John',
+        lastName: 'Smith',
+      },
+    }
+  },
+  computed: {
+    fullName() {
+      return this.user.firstName + ' ' + this.user.lastName
+    },
+  },
+}
+</script>
+
+<template>
+  <h1 :title="message" :class="{ red: isRed }">{{ title }}</h1>
+  <h2>{{ fullName }}さんのデータ</h2>
+  <p>Name: {{ fullName }}</p>
+</template>
